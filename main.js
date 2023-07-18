9432fbdce4b749b8b061d070021cec689372ba7d
hungmike <maingochungnuce@gmail.com>
2023-07-18 10:14:54 +0700

    complete camry

--- main.js
+++ main.js
@@ -7,7 +7,10 @@ const productTextColor = document.querySelector(
   ".products-page .product-detail .product-text-color"
 );
 
-// Lặp qua mỗi lựa chọn và thêm sự kiện click
+// Khi xem thêm câu hỏi thường gặp
+const questionCard = document.querySelectorAll(".questions .question-card");
+
+// 1. Khi chọn màu ô tô: Lặp qua mỗi lựa chọn và thêm sự kiện click
 for (let i = 0; i < colorOptions.length; i++) {
   colorOptions[i].addEventListener("click", function () {
     // Lấy đường dẫn ảnh và text
@@ -16,5 +19,34 @@ for (let i = 0; i < colorOptions.length; i++) {
     // Thay đổi ảnh và text
     productImage.src = selectedImage;
     productTextColor.innerHTML = selectedTextColor;
+  });
+}
+
+// 2. Khi xem thêm Câu hỏi thường gặp
+for (let i = 0; i < questionCard.length; i++) {
+  questionCard[i].addEventListener("click", function () {
+    const iconMinus = questionCard[i].querySelector(".fa-circle-minus");
+    const iconPlus = questionCard[i].querySelector(".fa-circle-plus");
+    const questionCardDes = questionCard[i].querySelector(
+      ".question-card__des"
+    );
+    const questionCardTitle = questionCard[i].querySelector(
+      ".question-card__title h4"
+    );
+
+    if (
+      questionCardDes.style.display === "none" ||
+      questionCardDes.style.display === ""
+    ) {
+      iconPlus.style.display = "none";
+      iconMinus.style.display = "block";
+      questionCardDes.style.display = "block";
+      questionCardTitle.classList.add("text-active");
+    } else {
+      iconPlus.style.display = "block";
+      iconMinus.style.display = "none";
+      questionCardDes.style.display = "none";
+      questionCardTitle.classList.remove("text-active");
+    }
   });
 }
